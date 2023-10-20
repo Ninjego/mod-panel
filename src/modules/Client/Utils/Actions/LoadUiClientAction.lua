603f852e042cdc2d203430476ae1321be882a31b
Ninjego <ninjego1@hotmail.com>
2023-10-21 00:24:15 +0200

    Player Count Ui

--- src/modules/Client/Utils/Actions/LoadUiClientAction.lua
+++ src/modules/Client/Utils/Actions/LoadUiClientAction.lua
@@ -3,6 +3,7 @@ local require = require(script.Parent.loader).load(script)
 local UpdateImageAction = require("UpdateImageClientAction")
 local UpdateTitleAction = require("UpdateTitleClientAction")
 local RegisterToggleAction = require("RegisterToggleClientAction")
+local UpdatePlayersAction = require("UpdatePlayersClientAction")
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
@@ -15,6 +16,7 @@ return {
         UpdateImageAction.execute(maid)
         UpdateTitleAction.execute(maid, player.Name)
         RegisterToggleAction.execute(maid)
+        UpdatePlayersAction.execute(maid)
         return "Success"
     end
 }
---
+++ src/modules/Client/Utils/Actions/UpdatePlayersClientAction.lua
@@ -0,0 +1,24 @@
+local require = require(script.Parent.loader).load(script)
+
+local Promise = require("Promise")
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+local player = Players.LocalPlayer
+local PlayerGui = player:WaitForChild("PlayerGui")
+
+local ClockClientUtil = require("ClockClientUtil")
+
+return {
+    args = {
+
+    },
+    execute = function()
+        local modUi = PlayerGui:FindFirstChild("ModUi")
+
+        if not(modUi) then
+            return "Unsuccessful"
+        end
+
+        modUi.Main.InformationFrame.PlayersFrame.ValueLabel.Text = #game:GetService("Players"):GetPlayers()
+    end
+}
